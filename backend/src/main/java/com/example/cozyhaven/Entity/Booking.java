9cea5990de13a423ce8e0175ca78b50ef97239ec
I <i@gmail.com>
2026-04-24 17:42:47 +0530

    Core Entities

--- backend/src/main/java/com/example/cozyhaven/Entity/Booking.java
+++ backend/src/main/java/com/example/cozyhaven/Entity/Booking.java
@@ -1,16 +1,38 @@
 package com.example.cozyhaven.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.cozyhaven.Enum.BookingStatus;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int bookingId;
+    private int adultCount;
+    private int childCount;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private LocalDate bookingDate;
+    private double totalAmount;
 
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId")
+    @JsonBackReference
+    private Customer customer;
+
+    @ManyToOne
+    private Room room;
 }
