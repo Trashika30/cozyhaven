69f8090d9e3bbaaccea6ee713b0c0fb9a77d26ee
trashika <trashikabaskaran@gmail.com>
2026-04-24 16:22:39 +0530

    entity hotel and owner class done

---
+++ backend/src/main/java/com/example/cozyhaven/Entity/Owner.java
@@ -0,0 +1,36 @@
+package com.example.cozyhaven.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Owner {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int ownerId;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private String contact;
+
+        @OneToMany(mappedBy = "owner",cascade = CascadeType.ALL)
+        List<Hotel> hotels;
+
+        public Owner( String firstName, String lastName, String email, String password, String contact) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.contact = contact;
+        }
+
+
+
+}
