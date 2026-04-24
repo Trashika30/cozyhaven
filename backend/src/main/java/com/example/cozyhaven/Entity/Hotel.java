b9b04f3e87e07ffbc67d21b58ec35fda87aa71e2
I <i@gmail.com>
2026-04-24 16:27:05 +0530

    ok

--- backend/src/main/java/com/example/cozyhaven/Entity/Hotel.java
+++ backend/src/main/java/com/example/cozyhaven/Entity/Hotel.java
@@ -9,41 +9,6 @@ import lombok.Data;
 @Entity
 @AllArgsConstructor
 public class Hotel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String hotelName;
-    private String description;
-    private boolean wifi;
-    private boolean parking;
-    private String roomService;
-    private String location;
-    private boolean pool;
-    private String contact;
-    private  String dining;
-    private boolean gym;
-
-    @ManyToOne
-    @JoinColumn(name="owner_id")
-    @JsonIgnore
-    private Owner owner;
-
-    public Hotel() {}
-
-    public Hotel(String hotelName, String description, boolean wifi, boolean parking, String roomService, String location, boolean pool, String contact, String dining, boolean gym,Owner owner) {
-        this.hotelName = hotelName;
-        this.description = description;
-        this.wifi = wifi;
-        this.parking = parking;
-        this.roomService = roomService;
-        this.location = location;
-        this.pool = pool;
-        this.contact = contact;
-        this.dining = dining;
-        this.gym = gym;
-        this.owner = owner;
-    }
-
 
 
 }
