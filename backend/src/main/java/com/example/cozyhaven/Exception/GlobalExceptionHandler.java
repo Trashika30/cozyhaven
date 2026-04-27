6d01e8824effeb21f523dafe1bac602c276892f2
I <i@gmail.com>
2026-04-27 12:41:25 +0530

    Mapper

--- backend/src/main/java/com/example/cozyhaven/Exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/cozyhaven/Exception/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.example.cozyhaven.Exception;
 
 
+import com.example.cozyhaven.ApiResponse.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 
@@ -15,19 +21,37 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Object>> handleNotFound(ResourceNotFoundException ex) {
 
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ApiResponse<>(false, ex.getMessage(), null));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
+                .body(new ApiResponse<>(ex.getMessage(), HttpStatus.NOT_FOUND, null));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException ex) {
-        String msg = ex.getBindingResult().getFieldError().getDefaultMessage();
-        return new ResponseEntity<>(msg, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse<Map<String, String>>>
+    handleValidationExceptions(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),
+                error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse<>("Invalid Input", HttpStatus.BAD_REQUEST, errors));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>>
+    handleValidationException(ConstraintViolationException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getConstraintViolations().forEach(error -> {
+            String field = error.getPropertyPath().toString();
+            String message = error.getMessage();
+            errors.put(field, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Object>> handleGeneral(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse<>(false, "Something went wrong", null));
+                .body(new ApiResponse<>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR, null));
     }
 }
