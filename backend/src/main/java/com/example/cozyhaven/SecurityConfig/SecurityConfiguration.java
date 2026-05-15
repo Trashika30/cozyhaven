34020499ab82552e036d8e9990ccf7fc90ba49fe
I <i@gmail.com>
2026-05-15 17:47:20 +0530

    blabla

--- backend/src/main/java/com/example/cozyhaven/SecurityConfig/SecurityConfiguration.java
+++ backend/src/main/java/com/example/cozyhaven/SecurityConfig/SecurityConfiguration.java
@@ -26,20 +26,13 @@ public class SecurityConfiguration {
         public CorsConfigurationSource corsConfigurationSource() {
 
                 CorsConfiguration configuration = new CorsConfiguration();
-
                 configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-
                 configuration.setAllowedMethods(
                                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
                 configuration.setAllowedHeaders(List.of("*"));
-
                 configuration.setAllowCredentials(true);
-
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
                 source.registerCorsConfiguration("/**", configuration);
-
                 return source;
         }
 
